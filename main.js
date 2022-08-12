cbb9f5ac008b009c346c83049cc1d79a8a3ba830
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-08-12 16:19:44 +0200

    javascript menu

---
+++ main.js
@@ -0,0 +1,8 @@
+ function showStarters() {
+    var start = document.getElementById("starters");
+    if(start.style.display === "none") {
+        start.style.display = "block";
+    }else{
+        start.style.display = "block ";
+    }
+ }
